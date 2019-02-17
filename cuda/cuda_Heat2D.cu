17dd7bfedf72864e5760c430581986770d2ae29c
kostasevan <sdi1400042@di.uoa.gr>
2019-02-17 17:48:48 +0200

    cuda

---
+++ cuda/cuda_Heat2D.cu
@@ -0,0 +1,20 @@
+/* =============================================
+ * Kostas Evangelou
+ * cuda_heat2D.c
+ * Parallel Systems
+ * CUDA Optimization of heat2D problem
+ * =============================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+__global__ void posibol(void){
+
+}
+
+int main(int argc, char *argv[]) {
+	printf("Hello World\n");
+	return 0;
+}
